f8c1410b36bb9c08c51610d3ce26a748c25fe5f8
Douglas Lima <douglasproglima@gmail.com>
2021-05-20 23:22:42 -0300

    Basic Models, Controllers and DB Migrations

---
+++ Backend/app/controllers/api/v1/available_cities_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::AvailableCitiesController < ApplicationController
+  def index
+  end
+end
---
+++ Backend/app/controllers/api/v1/orders_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::OrdersController < ApplicationController
+  def create
+  end
+
+  def show
+  end
+end
