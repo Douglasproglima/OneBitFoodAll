bf8d3d5e2da860b9ad64759fcca96e8ee6849db1
Douglas Lima <douglasproglima@gmail.com>
2021-05-25 22:15:54 -0300

    Change rails server use port 3001

--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -8,12 +8,3 @@ Rails.application.routes.draw do
     end
   end
 end
-
-#Rails.application.routes.draw do
-#  scope '/api/v1', defaults: { format: :json } do
-#    resources :categories, only: [:index]
-#    resources :restaurants, only: [:index, :show]
-#    resources :orders, only: [:create, :show]
-#    resources :available_cities, only: [:index]
-#  end
-#end
