e544e65dc484756e9f1299933919e0e69ceb2464
Douglasproglima <douglasproglima@gmail.com>
2021-05-26 22:56:06 -0300

    Initial Componente Restaurant and ListRestaurant

---
+++ frontend/components/ListRestaurants/index.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { Row, Col, Spinner, Alert } from 'react-bootstrap';
+import Restaurant from '../ListRestaurants/Restaurants';
+
+export default function ListRestaurants() {
+  return (
+    <div className='mt-5'>
+      <h3 className='fw-bold'>Restaurantes</h3>
+      <Row>
+        <Col>Lorem ipsum dolor sit amet.</Col>
+      </Row>
+    </div>
+  )
+}
