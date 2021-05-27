81cd0c9760b3b92150bd846a7384084c5375ef2c
Douglasproglima <douglasproglima@gmail.com>
2021-05-26 23:32:34 -0300

    Prepare Restaurant List Products

--- frontend/components/ListRestaurants/index.js
+++ frontend/components/ListRestaurants/index.js
@@ -1,14 +1,33 @@
 
 import React from 'react';
 import { Row, Col, Spinner, Alert } from 'react-bootstrap';
-import Restaurant from '../ListRestaurants/Restaurants';
+import Restaurant from '../ListRestaurants/Restaurant';
+
+const restaurants = [
+  {
+    'id': 1,
+    'name': 'Lorem ipsum dolor sit amet.',
+    'descrition': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, laudantium.',
+    'delivery_tax': '5',
+    'image_url': '/restaurant.jpeg',
+    'category_title': 'Cozinha Japonesa'
+  },
+  {
+    'id': 2,
+    'name': 'Lorem ipsum dolor sit amet.',
+    'descrition': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam, laudantium.',
+    'delivery_tax': '6',
+    'image_url': '/restaurant.jpeg',
+    'category_title': 'Cozinha Italiana'
+  }
+]
 
 export default function ListRestaurants() {
   return (
     <div className='mt-5'>
       <h3 className='fw-bold'>Restaurantes</h3>
       <Row>
-        <Col>Lorem ipsum dolor sit amet.</Col>
+        {restaurants.map((restaurant, i) => <Restaurant {...restaurant} key={i}/>)}
       </Row>
     </div>
   )
