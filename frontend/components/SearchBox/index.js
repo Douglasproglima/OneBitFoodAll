cb1d283dd7b0970180ec557c3312b00c1ce5058a
Douglasproglima <douglasproglima@gmail.com>
2021-06-03 10:07:05 -0300

    Change name parameter to the endPoint category

--- frontend/components/SearchBox/index.js
+++ frontend/components/SearchBox/index.js
@@ -5,20 +5,21 @@ import { useRouter } from 'next/router';
 
 export default function SearchBox() {
   const router = useRouter();
-  const [query, setQuery] = useState('');
+  const [q, setQuery] = useState('');
 
   const Search = (e) => {
     e.preventDefault();
-    router.push(`/restaurants?q=${query}`)
+    router.push(`/restaurants?q=${q}`)
   }
 
+  /*d-flex -> Coloca o botão e o input na mesma linha*/
   return (
     <Form className='d-flex mx-5 my-2' onSubmit={e => Search(e)}>
       <Form.Control
         type='text'
         placeholder='Buscar Restaurantes'
         className='me-2'
-        value={query}
+        value={q}
         onChange={e => setQuery(e.target.value)}
       />
       <Button variant='outline-custom-red' type='submit'>
