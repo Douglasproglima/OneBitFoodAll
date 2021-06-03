7ff16ab39a3b29158a0a70689bfaba7d4bbbe3e8
Douglasproglima <douglasproglima@gmail.com>
2021-06-02 21:54:52 -0300

    Add filter Category on Restaurants

--- frontend/services/api/getRestaurants.js
+++ frontend/services/api/getRestaurants.js
@@ -1,10 +1,20 @@
 import useSWR from 'swr';
+import { useRouter } from 'next/router';
 
 export default function getRestaurants() {
+  const router = useRouter();
+  const { category } = router.query; //Pega na URI
+
+  let params = '';
+  if(category) {
+    const questionPoint = `${params == '' ? '?' : '&'}`;
+    params = questionPoint + `category=${category}`;
+  }
+
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
   const { data, error } = useSWR(
-    `${process.env.apiUrl}/api/${process.env.version}/restaurants`,
+    `${process.env.apiUrl}/api/${process.env.version}/restaurants${params}`,
     fetcher,
     { revalidateOnFocus: false }
   )
